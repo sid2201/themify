804aef38ecc69a69e09d6934b6c09fcfb7532d4a
mpedrera <hola@manuelpedrera.com>
2013-12-16 20:57:38 +0100

    Added tests for Themify setTheme() and getTheme()

--- tests/ThemifyTest.php
+++ tests/ThemifyTest.php
@@ -1,20 +1,47 @@
 <?php
 
+use Mockery as M;
+use Mpedrera\Themify\Themify;
+
 class ThemifyTest extends PHPUnit_Framework_TestCase {
+
+    protected $resolver;
 
     public function setUp()
     {
-
+        $this->mockResolver();
     }
 
     public function tearDown()
     {
-
+        M::close();
     }
 
-    public function testFoo()
+    public function testThemeIsBeingSet()
     {
+        $t = new Themify($this->resolver);
+        $t->setTheme('footheme');
 
+        $this->assertEquals($t->getTheme(), 'footheme');
+    }
+
+    public function testReturnsResolvedThemeWhenOwnThemeIsNull()
+    {
+        $this->resolver->shouldReceive('resolve')
+            ->once()
+            ->andReturn('bartheme');
+
+        $t = new Themify($this->resolver);
+
+        $this->assertEquals($t->getTheme(), 'bartheme');
+    }
+
+    /**
+     *
+     */
+    protected function mockResolver()
+    {
+        $this->resolver = M::mock('Mpedrera\Themify\Resolver\Resolver');
     }
 
 }
