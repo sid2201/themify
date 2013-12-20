e5cbc6daee71cfcfaa585dbe3a73215c2cb855b7
Manuel Pedrera <hola@manuelpedrera.com>
2013-12-20 02:50:06 +0100

    Improved config.php documentation

--- src/config/config.php
+++ src/config/config.php
@@ -8,6 +8,16 @@ return array(
 
     // Internal folder where theme views are stored.
     // Defaults to (...)/app/themes
+    // 
+    // The package will search for views directly
+    // inside each theme folder, so you don't need
+    // a 'views' folder inside your theme:
+    // app
+    //   |_ themes
+    //      |_ default
+    //          |_ layouts
+    //          |_ post
+    //          |_ static
     'themes_path' => app_path() . '/themes',
 
     // The directory inside your public folder where all theme
@@ -16,5 +26,16 @@ return array(
     //
     // Do not include the public folder itself, or 
     // leading/trailing slashes.
+    // 
+    // With default value, your assets structure would be:
+    // public
+    // |_ assets
+    //    |_ themes
+    //        |_ default
+    //           |_ css, js, img, etc.
+    //        |_ admin-theme
+    //           |_ css, js, img, etc.
+    //        |_ alternative-theme
+    //           |_ css, js, img, etc.
     'themes_assets_path' => 'assets/themes',
 );
